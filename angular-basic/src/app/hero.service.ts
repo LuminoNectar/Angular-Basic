e60925a80e9f9e8e87ced6a479767e3184707bd8
John Williams <johnvictorwilliams@gmail.com>
2021-01-07 21:43:37 -0500

    Add message functionality to monitor function calls

--- angular-basic/src/app/hero.service.ts
+++ angular-basic/src/app/hero.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(private messageService: MessageService) { }
   getHeroes() {
     return of(HEROES);
     // return new Promise<Hero[]>(
     //   (resolve, reject) => resolve(HEROES) //Make this an asyncronise call
   }
-  constructor() { }
 }
